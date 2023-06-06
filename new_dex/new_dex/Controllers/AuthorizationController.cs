7f27540d41e2388562b349ea63e636c55f82a7a4
Kozmod <antoxa.kozmod@gmail.com>
2023-06-07 02:20:17 +0300

    Создание файла Exel в качестве бд. Успешная Регистрация

---
+++ new_dex/new_dex/Controllers/AuthorizationController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+
+public class AuthorizationController : Controller
+{
+    private readonly IWebHostEnvironment _hostingEnvironment;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AuthorizationController(IWebHostEnvironment hostingEnvironment, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _hostingEnvironment = hostingEnvironment;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Login(string login, string password)
+    {
+        // Путь к файлу Excel
+        string filePath = @"C:\Users\antox\source\repos\new_Dex\new_dex\Tabel\User.xlsx";
+
+        // Чтение данных из файла Excel
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            if (worksheet != null)
+            {
+                // Поиск соответствия логина и пароля в файле Excel
+                int rowCount = worksheet.Dimension.Rows;
+                for (int row = 2; row <= rowCount; row++) // Начинаем с 2 строки, так как первая строка содержит заголовки
+                {
+                    string excelLogin = worksheet.Cells[row, 1]?.Value?.ToString();
+                    string excelPassword = worksheet.Cells[row, 2]?.Value?.ToString();
+
+                    if (excelLogin == login && excelPassword == password)
+                    {
+                        // Логин и пароль найдены, перенаправляем на главное меню
+                        return RedirectToAction("MainMenu", "Home");
+                    }
+                }
+            }
+        }
+
+        // Логин и пароль не найдены, отображаем ошибку
+        ViewBag.ErrorMessage = "Неверный логин или пароль";
+        return View("Login");
+    }
+
+
+
+}
---
+++ new_dex/new_dex/Controllers/RegistrationController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.IO;
+
+public class RegistrationController : Controller
+{
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public RegistrationController(IWebHostEnvironment hostingEnvironment)
+    {
+        _hostingEnvironment = hostingEnvironment;
+    }
+
+    [HttpPost]
+    public IActionResult Register(string login, string password, string email)
+    {
+        // Путь к файлу Excel
+        string filePath = @"C:\Users\antox\source\repos\new_Dex\new_dex\Tabel\User.xlsx";
+
+        // Установка контекста лицензирования
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        // Создание нового пакета Excel
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            if (worksheet == null)
+            {
+                // Если лист не существует, создаем новый
+                worksheet = package.Workbook.Worksheets.Add("Users");
+                // Добавляем заголовки столбцов
+                worksheet.Cells[1, 1].Value = "Логин";
+                worksheet.Cells[1, 2].Value = "Пароль";
+                worksheet.Cells[1, 3].Value = "Email";
+            }
+
+            // Получение последней заполненной строки в Excel
+            int lastRow = worksheet.Dimension.Rows;
+
+            // Определение следующей строки для записи данных
+            int nextRow = lastRow + 1;
+
+            // Запись данных в Excel
+            worksheet.Cells[nextRow, 1].Value = login;
+            worksheet.Cells[nextRow, 2].Value = password;
+            worksheet.Cells[nextRow, 3].Value = email;
+
+            // Сохранение пакета Excel
+            package.Save();
+        }
+
+        // Дополнительный код после сохранения данных
+
+        return RedirectToAction("Index", "Home"); // Перенаправление на другую страницу
+    }
+
+}
+
