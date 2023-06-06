df8e80ddca888c23786c46db1be2d5c2793c9e4d
Ksusha4337 <FedotovaKA@stud.kai.ru>
2023-06-06 14:20:20 +0300

    Начинаем заново работать

---
+++ new_dex/new_dex/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using new_dex.Models;
+using System.Diagnostics;
+
+namespace new_dex.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Переход на страницу Регистриации
+        public IActionResult RegistrationWindow()
+        {
+            return View("RegistrationWindow"); // Переход на представление "Registration"
+        }
+
+        //Переход на страницу Авторизации
+        public IActionResult GoToSign()
+        {
+            return View("Index"); // Переход на действие "Index" (страницу "Авторизация")
+        }
+        public IActionResult MainMenu()
+        {
+            return View("MainMenu"); // Переход на действие "Index" (страницу "Авторизация")
+        }
+    }
+}
